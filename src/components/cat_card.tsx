8b00c0969006272aed2c66aa06aa1a82a7d2f3c2
cj-rupp <72820354+cj-rupp@users.noreply.github.com>
2023-03-14 16:37:05 +0000

    fix: removed redundant imports

--- src/components/cat_card.tsx
+++ src/components/cat_card.tsx
@@ -1,5 +1,3 @@
-import { Fragment } from 'react';
-import Cat from '../data/cat';
 import cat1 from '../assets/images/cat1.jpg';
 import cat2 from '../assets/images/cat2.jpg';
 import cat3 from '../assets/images/cat3.jpg';
